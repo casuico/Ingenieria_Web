b45e26c63bbe99ab4ce4b0a62243e89d94424f3f
nicol <nicolas_clementz@hotmail.com>
2025-08-18 00:42:00 -0300

    build y run scripts

--- build.sh
+++ build.sh
@@ -2,15 +2,8 @@
 set -o errexit
 
 # install project dependencies
-#uv sync
 pip install -r requirements.txt
 
-# make sure django has all the things it needs to run
-cd TP_Adopciones
-
 # preparar Django
+cd TP_Adopciones
 python manage.py collectstatic --no-input
-python manage.py migrate
-
-# crear superusuario si no existe
-python manage.py createsuperuser --username admin --email "nicolas_clementz@hotmail.com" --noinput || true
--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 # exit on error
 set -o errexit
 
-# run the web app server
 cd TP_Adopciones
+
+# correr migraciones en el contenedor ya conectado a la DB
+python manage.py migrate
+
+# crear superusuario si no existe
+python manage.py createsuperuser --username admin --email "nicolas_clementz@hotmail.com" --noinput || true
+
+# arrancar el servidor
 gunicorn adopciones_project.wsgi:application
