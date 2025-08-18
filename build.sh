546484e4093652f3a5835c294de16d462b987f13
nicol <nicolas_clementz@hotmail.com>
2025-08-18 00:58:44 -0300

    removed media from gitignore, run, build scripts, added image

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 set -o errexit
 
 # install project dependencies
+#uv sync
 pip install -r requirements.txt
 
-# preparar Django
+# make sure django has all the things it needs to run
 cd TP_Adopciones
+
+# preparar Django
 python manage.py collectstatic --no-input
+python manage.py migrate
+
+# crear superusuario si no existe
+python manage.py createsuperuser --username admin --email "nicolas_clementz@hotmail.com" --noinput || true
