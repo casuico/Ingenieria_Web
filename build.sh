83f5f92a9089ca69acfe5996c06bfdcd2de3e314
nicol <nicolas_clementz@hotmail.com>
2025-08-18 22:11:38 -0300

    build error x5

--- build.sh
+++ build.sh
@@ -7,13 +7,20 @@ pip install -r requirements.txt
 
 # make sure django has all the things it needs to run
 cd TP_Adopciones
+
 echo "DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-not set}"
 echo "Python version:"
 python --version
 pip list
 
 export DJANGO_SETTINGS_MODULE=adopciones_project.settings
+export PYTHONPATH=$PWD
 echo "DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE"
+echo "PYTHONPATH=$PYTHONPATH"
+
+# chequear que Django carga correctamente
+python manage.py check
+
 # preparar Django
 python manage.py collectstatic --no-input --verbosity 2
 python manage.py migrate
