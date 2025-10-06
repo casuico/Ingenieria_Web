03d636ad8f5395e47b3c72c1e8d1f8c29a704183
CesarWer <werlencesar@gmail.com>
2025-10-06 17:20:30 -0300

    Update signals.py

--- TP_Adopciones/adopcionesapp/signals.py
+++ TP_Adopciones/adopcionesapp/signals.py
@@ -1,19 +1,16 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from haystack import connections
-from haystack import indexes
 from .models import Publicacion
-
 
 @receiver(post_save, sender=Publicacion) 
 def update_publicacion_index(sender, instance, **kwargs):
     backend = connections['default'].get_backend()
-    index = indexes.get_index(sender)
+    index = connections['default'].get_unified_index().get_index(sender)
     backend.update(index, [instance])
-
 
 @receiver(post_delete, sender=Publicacion)
 def remove_publicacion_index(sender, instance, **kwargs):
     backend = connections['default'].get_backend()
-    index = indexes.get_index(sender)
-    backend.remove(index, [instance]) 
+    index = connections['default'].get_unified_index().get_index(sender)
+    backend.remove(index, [instance])
