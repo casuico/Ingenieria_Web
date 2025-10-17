35a80c74031c428cc5dca4b629cc9bfe51912188
nicol <nicolas_clementz@hotmail.com>
2025-10-17 16:46:34 -0300

    el signals rompia cuando se creaba una publicacion o marcar como adoptado. + mostrar las publicaciones en la pagina de perfil con se hace en la home

--- TP_Adopciones/adopcionesapp/signals.py
+++ TP_Adopciones/adopcionesapp/signals.py
@@ -1,17 +1,17 @@
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
-from haystack import connections
-from .models import Publicacion
+# from django.db.models.signals import post_save, post_delete
+# from django.dispatch import receiver
+# from haystack import connections
+# from .models import Publicacion
 
 
-@receiver(post_save, sender=Publicacion)
-def update_publicacion_index(sender, instance, **kwargs):
-    backend = connections['default'].get_backend()
-    backend.update(Publicacion, [instance]) 
+# @receiver(post_save, sender=Publicacion)
+# def update_publicacion_index(sender, instance, **kwargs):
+#     backend = connections['default'].get_backend()
+#     backend.update(Publicacion, [instance]) 
 
 
-@receiver(post_delete, sender=Publicacion)
-def remove_publicacion_index(sender, instance, **kwargs):
-    backend = connections['default'].get_backend()
-    backend.remove(instance) 
+# @receiver(post_delete, sender=Publicacion)
+# def remove_publicacion_index(sender, instance, **kwargs):
+#     backend = connections['default'].get_backend()
+#     backend.remove(instance) 
 
